8c6485c5d431e503c0b768ec7a8e2dc96b74deea
Amila Welihinda <amilajack@gmail.com>
2017-03-28 10:04:16 -0700

    Revised e2e tests

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import type { NodePath } from 'babel-traverse';
 
 /* eslint no-restricted-syntax: 0, fp/no-loops: 0, no-continue: 0, func-names: 0, fp/no-let: 0 */
 
-
 // https://astexplorer.net/#/gist/a6acab67ec110ce0ebcfbbee7521de2a/779ae92132ef8c26e0b8f37e96ec83ab176d33f3
 
 export default function ({ types: t }) {
   return {
     visitor: {
       Program(path: NodePath) {
+        // ES6 Imports
+        // state.file.addImport('safe-access-chech', 'safeCoerce');
+        // state.file.addImport('safe-access-chech', 'safePropertyAccess');
+
+        // CommonJS Imports
         path.unshiftContainer('body', [
           t.variableDeclaration('var', [
             t.variableDeclarator(
@@ -30,7 +34,19 @@ export default function ({ types: t }) {
         ]);
       },
 
-      MemberExpression(path) {
+      MemberExpression(path, state) {
+        if (t.isAssignmentExpression(path.parent)) {
+          if (path.parentKey === 'left') {
+            return;
+          }
+        }
+
+        // @HACK: Temporarily prevent wrapping callExpression's
+        //        Reason of bug is unknown
+        if (t.isCallExpression(path.parent)) {
+          return;
+        }
+
         let object = path.node;
         const items = [];
         let id;
@@ -51,17 +67,46 @@ export default function ({ types: t }) {
           }
         }
 
-        path.replaceWith(
-          t.callExpression(
-            t.identifier('safePropertyAccess'),
-            [
-              t.arrayExpression(
-                items.reverse()
-              ),
-              t.identifier(id.name || id.callee.name)
-            ]
-          )
-        );
+        if (!id) {
+          return;
+        }
+        if (!(id.name)) {
+          if (!id.callee) {
+            return;
+          }
+        } else if (!id.callee) {
+          if (!id.name) {
+            return;
+          }
+        }
+
+        try {
+          if (!(id.name || id.callee.name)) {
+            return;
+          }
+        } catch (e) {
+          return;
+        }
+
+        try {
+          path.replaceWith(
+            t.callExpression(
+              t.identifier('safePropertyAccess'),
+              [
+                t.arrayExpression(
+                  items.reverse()
+                ),
+                t.identifier(id.name || id.callee.name)
+              ]
+            )
+          );
+        } catch (error) {
+          throw new Error([
+            'This is an issue with "babel-plugin-fail-explicit"',
+            `Line "${path.node.loc.start}" in "${state.file.opts.filename}"`,
+            error
+          ].join('. '));
+        }
       },
 
       /**
